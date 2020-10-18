d8f3726ab3633434c73594669d28684c01fec51b
paolaozv <paolaozv@gmail.com>
2020-10-18 21:49:24 +0200

    added proxy and changed url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,8 +6,12 @@ import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
+// const httpLink = createHttpLink({
+//   uri: 'http://localhost:5000'
+// });
+
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://serene-dusk-53077.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
